25227b3f55ef8377e18c5346604e0e37322b550f
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2025-12-02 18:48:42 +0100

    Update listarProfesores.php

--- Profesores/vistas/listarProfesores.php
+++ Profesores/vistas/listarProfesores.php
@@ -6,8 +6,13 @@
     <link rel="stylesheet" href="css/estilo.css">
 </head>
 <body>
+
     <h1>Profesores</h1>
-    <a href="index.php?c=ProfesorControlador&m=mostrarCrear" class="boton-anadir">Añadir Profesor</a>   
+
+    <a href="index.php?c=ProfesorControlador&m=mostrarCrear" class="boton-anadir">
+        Añadir Profesor
+    </a>
+
     <table class="tabla-datos">
         <thead>
             <tr>
@@ -16,27 +21,30 @@
             </tr>
         </thead>
         <tbody>
-            <?php if (!empty($datos)) { ?>
-            <?php foreach ($datos as $fila) { ?>
-                <tr>
-                    <td><?php echo $fila['nombre']; ?></td>
-                    <td>
-                        <form action="index.php?c=ProfesorControlador&m=mostrarEditar" method="POST">
-                            <input type="hidden" name="id" value="<?php echo $fila['idProfesor']; ?>">
-                            <input type="submit" value="Editar">
-                        </form>
+        <?php
+            if (!empty($datos)) {
+                foreach ($datos as $fila){
+                    echo "<tr>";
+                    echo "<td>" . $fila['nombre'] . "</td>";
+                    echo "<td>
+                            <form action='index.php?c=ProfesorControlador&m=mostrarEditar' method='POST'>
+                                <input type='hidden' name='id' value='" . $fila['idProfesor'] . "'>
+                                <input type='submit' value='Editar'>
+                            </form>
 
-                        <form action="index.php?c=ProfesorControlador&m=confirmarEliminar" method="POST">
-                            <input type="hidden" name="id" value="<?php echo $fila['idProfesor']; ?>">
-                            <input type="submit" value="Eliminar">
-                        </form>
-                    </td>
-                </tr>
-            <?php } ?>
-        <?php } else { ?>
-            <tr><td colspan="2">No hay datos</td></tr>
-        <?php } ?>
+                            <form action='index.php?c=ProfesorControlador&m=confirmarEliminar' method='POST'>
+                                <input type='hidden' name='id' value='" . $fila['idProfesor'] . "'>
+                                <input type='submit' value='Eliminar'>
+                            </form>
+                         </td>";
+                    echo "</tr>";
+                }
+            }
+            else{
+                echo "<tr><td colspan='2'>No hay datos</td></tr>";
+            }
+        ?>
         </tbody>
-    </table> 
+    </table>
 </body>
 </html>
