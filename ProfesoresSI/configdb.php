419d3ec46b2d73e046509f43d3602a41cd8871a6
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2025-12-05 20:19:22 +0100

    Delete ProfesoresSI directory

--- ProfesoresSI/configdb.php
+++
@@ -1,6 +0,0 @@
-<?php
-define("SERVIDOR", "localhost");
-define("USUARIO", "root");
-define("CLAVE", ""); 
-define("NOMBRE_BD", "clase");
-?>
--- ProfesoresSI/listar.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once 'Controlador/ProfesorControlador.php';
-
-$objControlador = new ProfesorControlador();
-
-$datos = $objControlador->listar();
-
-include 'vistas/listarProfesores.php';
-?>
