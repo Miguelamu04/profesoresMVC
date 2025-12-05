6787be5dde52e9b37af6a4ddc215d8e358cea5ac
Miguel <miguelalbencamunoz.guadalupe@alumnado.fundacionloyola.net>
2025-12-05 20:18:31 +0100

    Delete Profesores directory

--- Profesores/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Controlador y método por defecto
-define('CONTROLADOR_USUARIO', 'ProfesorControlador');
-define('METODO_PREDETERMINADO', 'listar');
-
-// Rutas correctas según tu estructura y la ubicación de index.php
-define('RUTA_CONTROLADORES', __DIR__ . '/Controlador/');
-define('RUTA_VISTAS', __DIR__ . '/vistas/');
-?>
